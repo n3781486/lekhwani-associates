d954dabc0057e0970fff884215d7636fa665ee78
n3781486 <n3781486@gmail.com>
2026-05-15 22:12:29 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./layout/Mainlayout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
